2eca72ed275acfa675e2a1e2884151aa5a2c3f41
Banna9898 <68413142+Banna9898@users.noreply.github.com>
2024-07-25 09:23:07 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,13 +15,13 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None)
-SUDO_USERS = []
+API_ID = "29141829"
+API_HASH = "25020d00fbcfd406fc9979d24d761bff"
+BOT_TOKEN = "6998826537:AAE24A88dHWSxFLFQ5jFYs2Sq8UwDPtasug"
+SESSION = "BQG8q0UAEbO1nOy4X5K_0Y_2iy72Yh42wPf_wA08yUZvK9GmSNJMrscxcYkr7fURZOLnZlgbVUDnYjXjVCNDGDlyQdocL4B3XyszSIW-iUGMsgLPlvkkEK_xQyns_iJFKlcQDJ3ER_BDIGbH5SxoRLkZvKTwnVmC3x8MY51ITbGXYeUQzsF2lKHPxvUaTOtH-VPsNy11oqfJHWKv7XFLB7gtgZ5-NJkH5AA-gpew8NMCO8xyw98_xUsmjsTuIbxGazXv7G1tKEwCPRzoJuL3Xv5eNXAldb8bpzh9UGFq3y93E0COjvgnCeNUmZAL9N7o3xkbUijMgC1YExUcz2fYMen7KixX9QAAAAE9TLQaAA"
+FORCESUB = "jayhind_675"
+AUTH = "5323404314"
+SUDO_USERS = "5323404314"
 
 if len(AUTH) != 0:
     SUDO_USERS = {int(AUTH.strip()) for AUTH in AUTH.split()}
@@ -35,7 +35,7 @@ userbot = Client("myacc",api_id=API_ID,api_hash=API_HASH,session_string=SESSION)
 try:
     userbot.start()
 except BaseException:
-    print("Your session expired please re add that... thanks @dev_gagan.")
+    print("Your session expired please re add that... thanks @galib_shayar.")
     sys.exit(1)
 
 Bot = Client(
